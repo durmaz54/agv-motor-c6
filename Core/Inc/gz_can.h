63c6642bbf8af5bb1100393a711c574bea18b619
durmaz54 <abdulsametdurmaz54@gmail.com>
2023-04-17 00:18:51 +0300

    init c6

---
+++ Core/Inc/gz_can.h
@@ -0,0 +1,51 @@
+
+
+#ifndef INC_GZ_CAN_H_
+#define INC_GZ_CAN_H_
+
+/*
+ * gz_can.h
+ *
+ *  Created on: 12 Kas 2022
+ *      Author: Abdul Samet Durmaz
+ *
+ *
+ *CAN RX0 and CAN SCE interrupt açık olmalı
+ *CAN
+*/
+
+
+#include "stm32f1xx_hal.h"
+#include "stdlib.h"
+
+
+#define CAN_DEADTIME 3000 //millisecond
+
+#define MYSTDID 0x010
+//10
+#define OTHERSTDID 0x016
+
+extern CAN_HandleTypeDef hcan;
+
+extern void Error_Handler(void);
+
+struct MOTOR{
+	int8_t temp;
+	uint8_t voltage;
+	uint8_t current;
+
+};
+
+void GZ_CAN_Init();
+
+void GZ_CAN_Transmit(struct MOTOR motorx);
+
+double GZ_CAN_Receive();
+
+
+
+
+
+
+
+#endif /* INC_GZ_CAN_H_ */
---
+++ Core/Src/gz_can.c
@@ -0,0 +1,103 @@
+/*
+ * gz_can.c
+ *
+ *  Created on: 12 Kas 2022
+ *      Author: Abdul Samet Durmaz
+ */
+
+#include "gz_can.h"
+
+
+
+static CAN_TxHeaderTypeDef myTxHeader;
+static CAN_RxHeaderTypeDef myRxHeader;
+static uint8_t rxData[8] = { 0 };
+static int32_t nowTime=0, dTime=0;
+
+
+void GZ_CAN_Init() {
+
+	CAN_FilterTypeDef canfilterconfig;
+
+	canfilterconfig.FilterActivation = CAN_FILTER_ENABLE;
+	canfilterconfig.FilterBank = 10;
+	canfilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+	// değişken 16, id 11 bit olduğu için kaydırıyoruz.
+	canfilterconfig.FilterIdHigh = OTHERSTDID << 5;
+	canfilterconfig.FilterIdLow = 0x0000; // for ext id
+	canfilterconfig.FilterMaskIdHigh = 0xFFFF << 5;
+	canfilterconfig.FilterMaskIdLow = 0xFFFF; // for ext id
+
+	canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
+	canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
+	canfilterconfig.SlaveStartFilterBank = 10;
+
+
+	if (HAL_CAN_ConfigFilter(&hcan, &canfilterconfig) != HAL_OK) {
+		Error_Handler();
+	}
+
+	if (HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+
+	//CAN hata durumunda bir kere interrupt girmesi için CAN_IT_ERROR_WARNING
+	//hata boyunca interrupta kalması için CAN_IT_LAST_ERROR_CODE
+	if (HAL_CAN_ActivateNotification(&hcan,
+			CAN_IT_ERROR | CAN_IT_LAST_ERROR_CODE) != HAL_OK) {
+		Error_Handler();
+	}
+
+	if (HAL_CAN_Start(&hcan) != HAL_OK) {
+		Error_Handler();
+	}
+
+	myTxHeader.DLC = 8; // max byte
+	myTxHeader.StdId = MYSTDID;
+	myTxHeader.RTR = CAN_RTR_DATA;
+	myTxHeader.IDE = CAN_ID_STD;
+
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &myRxHeader, rxData)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (myRxHeader.StdId != OTHERSTDID) {
+		Error_Handler();
+	}
+	nowTime = HAL_GetTick();
+
+}
+
+void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan) {
+
+	//hata logları
+
+}
+
+double GZ_CAN_Receive() {
+	dTime = HAL_GetTick();
+	double data = atof(rxData);
+
+	if(dTime-nowTime > CAN_DEADTIME){
+		return 0x00;
+	}
+
+	return data;
+}
+
+void GZ_CAN_Transmit(struct MOTOR motorx) {
+	uint32_t txmailbox;
+	uint8_t txdata[8];
+
+	txdata[0] = motorx.temp;
+	txdata[1] = motorx.voltage;
+	txdata[2] = motorx.current;
+	myTxHeader.DLC = 3;
+	HAL_CAN_AddTxMessage(&hcan, &myTxHeader, txdata, &txmailbox);
+
+}
